bd0ed13ca78ed0b2ea18ee6323953cd59add2158
wandoy37 <riswandi035@gmail.com>
2022-05-26 00:18:36 +0800

    Integration View About

--- resources/views/Home/about.blade.php
+++ resources/views/Home/about.blade.php
@@ -8,7 +8,7 @@
             <div class="col-lg-8 col-xxl-6">
                 <div class="text-center my-5">
                     <h1 class="fw-bolder mb-3">About Us</h1>
-                    <p class="lead fw-normal text-muted mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                    <p class="lead fw-normal text-muted mb-4">{{ $about->description }}</p>
                 </div>
             </div>
         </div>
@@ -26,7 +26,7 @@
             <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="{{ asset('home/img/vision.png') }}" alt="..." /></div>
             <div class="col-lg-6">
                 <h2 class="fw-bolder">Visi</h2>
-                <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                <p class="lead fw-normal text-muted mb-0">{{ $about->visi }}</p>
             </div>
         </div>
     </div>
@@ -38,7 +38,7 @@
             <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="{{ asset('home/img/mision.png') }}" alt="..." /></div>
             <div class="col-lg-6">
                 <h2 class="fw-bolder">Misi</h2>
-                <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                <p class="lead fw-normal text-muted mb-0">{{ $about->misi }}</p>
             </div>
         </div>
     </div>
@@ -47,20 +47,23 @@
 <section class="py-5 bg-light">
     <div class="container px-5 my-5">
         <div class="row gx-5 row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center">
+            @foreach ($team as $team)
             <div class="col mb-5 mb-5 mb-xl-0">
                 <div class="text-center">
-                    <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
-                    <h5 class="fw-bolder">Ibbie Eckart</h5>
-                    <div class="fst-italic text-muted">Founder &amp; CEO</div>
+                    <img class="img-fluid rounded-circle mb-4 px-4" src="{{ $team->image }}" alt="..." />
+                    <h5 class="fw-bolder">{{ $team->name }}</h5>
+                    <div class="fst-italic text-muted">{{ $team->position }}</div>
                 </div>
             </div>
-            <div class="col mb-5 mb-5 mb-xl-0">
+            @endforeach
+            
+            {{-- <div class="col mb-5 mb-5 mb-xl-0">
                 <div class="text-center">
                     <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
                     <h5 class="fw-bolder">Arden Vasek</h5>
                     <div class="fst-italic text-muted">CFO</div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </section>
